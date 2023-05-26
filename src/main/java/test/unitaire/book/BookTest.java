dae9a51e6cace786024082790a84e610f623939e
purty12 <91609727+purty12@users.noreply.github.com>
2023-05-26 15:24:06 +0200

    test

--- src/main/java/test/unitaire/book/BookTest.java
+++ src/main/java/test/unitaire/book/BookTest.java
@@ -3,38 +3,30 @@ package test.unitaire.book;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Date;
+import java.util.stream.Stream;
 
 public class BookTest {
-    @ParameterizedTest
-    @CsvSource({
-            "Title 1, Author 1, 2021-01-01",
-            "Title 2, Author 2, 2022-02-02",
-            "Title 3, Author 3, 2023-03-03"
-    })
-    public void testGetTitle(String title, String author, String publicationDate) {
-        Date date = parsePublicationDate(publicationDate);
-        Book book = new Book(title, author, date);
-        Assertions.assertEquals(title, book.getTitle());
+    private static Stream<Book> bookProvider() {
+        return Stream.of(
+                new Book("Title 1", "Author 1", new Date()),
+                new Book("Title 2", "Author 2", new Date()),
+                new Book("Title 3", "Author 3", new Date())
+        );
     }
 
     @ParameterizedTest
-    @CsvSource({
-            "Title 1, Author 1, 2021-01-01",
-            "Title 2, Author 2, 2022-02-02",
-            "Title 3, Author 3, 2023-03-03"
-    })
-    public void testGetAuthor(String title, String author, String publicationDate) {
-        Date date = parsePublicationDate(publicationDate);
-        Book book = new Book(title, author, date);
-        Assertions.assertEquals(author, book.getAuthor());
+    @MethodSource("bookProvider")
+    public void testGetTitle(Book book) {
+        Assertions.assertNotNull(book.getTitle());
     }
 
-    private Date parsePublicationDate(String publicationDate) {
-        // Méthode pour convertir la date de publication en objet Date
-        // Implémentation spécifique à votre cas
-        // Retourne la date parsée
-        return null;
+    @ParameterizedTest
+    @MethodSource("bookProvider")
+    public void testGetAuthor(Book book) {
+        Assertions.assertNotNull(book.getAuthor());
     }
+
 }
